f013023484d6485b6bf3ea374d66593baf024e05
yubinquitous <yb9854@naver.com>
2024-01-02 13:55:45 +0900

    feat: p1

---
+++ p1/scripts/Client-Script.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+sudo apt-get update
+sudo apt-get upgrade -y
+# docker init
+
+#sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common 
+#sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+#sudo apt-get update
+#
+#sudo apt-get install -y docker-ce docker-ce-cli containerd.io 
+# k3s init
+export INSTALL_K3S_EXEC="agent --server https://192.168.56.110:6443 --token-file /vagrant/node-token --node-ip 192.168.56.111"
+curl -sfL https://get.k3s.io | sh - 
