943e34865f048fa2addf4f3b7065deb286f71360
yubinquitous <yb9854@naver.com>
2024-01-13 15:08:03 +0900

    feat: p2, p3

--- p2/scripts/Client-Script.sh
+++ p2/scripts/Client-Script.sh
@@ -1,9 +1,7 @@
 #! /bin/bash
 
-# vagrant up 속도 향샹을 위해 주석 처리
 sudo apt-get update
 sudo apt-get upgrade -y
-
 # docker init
 
 #sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common 
@@ -12,5 +10,11 @@ sudo apt-get upgrade -y
 #
 #sudo apt-get install -y docker-ce docker-ce-cli containerd.io 
 # k3s init
-export INSTALL_K3S_EXEC="agent --server https://192.168.56.110:6443 --token-file /vagrant/node-token --node-ip 192.168.56.111"
-curl -sfL https://get.k3s.io | sh - 
+#sudo ufw disable
+#export INSTALL_K3S_EXEC="agent --server https://192.168.56.110:6443 --token-file /vagrant/node-token --node-ip 192.168.56.111"
+
+#curl -sfL https://get.k3s.io | sh - 
+#mkdir -p ${HOME}/.kube
+#cp /vagrant/k3s.yaml .kube/config
+#chmod 600 ~/.kube/config
+#export KUBECONFIG=~/.kube/config
