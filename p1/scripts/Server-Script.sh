c86b4480b03bf6cb6b394fbf202e1e94694985e7
yubinquitous <yb9854@naver.com>
2024-01-13 17:34:57 +0900

    fix: p1 Vagrantfile provision 에러 수정

--- p1/scripts/Server-Script.sh
+++ p1/scripts/Server-Script.sh
@@ -13,7 +13,7 @@ sudo apt-get upgrade -y
 
 # k3s init
 #sudo apt-get install cgroup-bin
-export INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --tls-san $(hostname) --node-ip 192.168.56.110 --bind-address 192.168.56.110  --advertise-address 192.168.56.110"
+export INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --tls-san $(hostname) --node-ip 192.168.56.110 --bind-address 192.168.56.110 --advertise-address 192.168.56.110"
 curl -sfL https://get.k3s.io | sh -
 sudo chmod 644 /etc/rancher/k3s/k3s.yaml
 sudo cp /etc/rancher/k3s/k3s.yaml /vagrant
