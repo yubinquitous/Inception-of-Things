5fac9ac6ca60f61ce5e04b7318d36ed63d1cad97
yubinquitous <yb9854@naver.com>
2024-01-13 15:14:20 +0900

    fix: p1 arm 환경 빌드를 위해 수정

--- p1/scripts/Client-Script.sh
+++ p1/scripts/Client-Script.sh
@@ -11,4 +11,5 @@ sudo apt-get upgrade -y
 #sudo apt-get install -y docker-ce docker-ce-cli containerd.io 
 # k3s init
 export INSTALL_K3S_EXEC="agent --server https://192.168.56.110:6443 --token-file /vagrant/node-token --node-ip 192.168.56.111"
+mkdir -p $HOME/.kube
 curl -sfL https://get.k3s.io | sh - 
--- p1/scripts/Server-Script.sh
+++ p1/scripts/Server-Script.sh
@@ -16,4 +16,5 @@ sudo apt-get upgrade -y
 export INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --tls-san $(hostname) --node-ip 192.168.56.110 --bind-address 192.168.56.110  --advertise-address 192.168.56.110"
 curl -sfL https://get.k3s.io | sh -
 sudo chmod 644 /etc/rancher/k3s/k3s.yaml
+sudo cp /etc/rancher/k3s/k3s.yaml /vagrant
 sudo cp /var/lib/rancher/k3s/server/node-token /vagrant/
